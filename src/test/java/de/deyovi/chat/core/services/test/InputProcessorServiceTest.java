3cb51923523065cf19d51fb697169f1d6ccd77f0
Michael Feige <Michael.Feige@hmmh.de>
2013-09-09 14:12:21 +0200

    unit-test for input-processing

---
+++ src/test/java/de/deyovi/chat/core/services/test/InputProcessorServiceTest.java
@@ -0,0 +1,60 @@
+package de.deyovi.chat.core.services.test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.deyovi.chat.core.objects.Segment;
+import de.deyovi.chat.core.objects.Segment.ContentType;
+import de.deyovi.chat.core.services.InputProcessorService;
+import de.deyovi.chat.core.services.impl.DefaultInputProcessorService;
+import de.deyovi.chat.core.utils.ChatConfiguration;
+
+public class InputProcessorServiceTest {
+
+	private InputProcessorService service;
+	
+	@Before
+	public void setup() {
+		ChatConfiguration.initialize();
+		service = DefaultInputProcessorService.getInstance();
+	}
+	
+	@Test
+	public void testSimpleStringOutput() {
+		Segment[] process = service.process(null, "Hallo Welt", null, null);
+		assertEquals("Hallo Welt", process[0].getContent());
+	}
+	
+	@Test
+	public void testSimpleURL() {
+		Segment[] process = service.process(null, "http://www.google.de", null, null);
+		assertEquals("http://www.google.de", process[0].getContent());
+		assertEquals(ContentType.WEBSITE, process[0].getType());
+	}
+	
+	@Test
+	public void testSimpleImage() {
+		Segment[] process = service.process(null, "https://www.google.de/images/srpr/logo4w.png", null, null);
+		assertEquals("https://www.google.de/images/srpr/logo4w.png", process[0].getContent());
+		assertEquals(ContentType.IMAGE, process[0].getType());
+	}
+	
+
+	@Test
+	public void testTextWebsiteAndImage() {
+		Segment[] process = service.process(null, "Hallo auf http://www.google.de gibt es ein Bild http://www.google.de/images/srpr/logo4w.png das ist toll", null, null);
+		assertEquals("Hallo auf", process[0].getContent());
+		assertEquals(ContentType.TEXT, process[0].getType());
+		assertEquals("http://www.google.de", process[1].getContent());
+		assertEquals(ContentType.WEBSITE, process[1].getType());
+		assertEquals("gibt es ein Bild", process[2].getContent());
+		assertEquals(ContentType.TEXT, process[2].getType());
+		assertEquals("http://www.google.de/images/srpr/logo4w.png", process[3].getContent());
+		assertEquals(ContentType.IMAGE, process[3].getType());
+		assertEquals("logo4w.png", process[3].getAlternateName());
+		assertEquals("das ist toll", process[4].getContent());
+		assertEquals(ContentType.TEXT, process[4].getType());
+	}
+	
+}
